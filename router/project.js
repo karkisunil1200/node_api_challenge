438a7e661c885d0be342944abbd5667e57377252
Sunil Karki <karkisunil1200@gmail.com>
2020-05-08 11:49:27 -0500

    Project CRUD functionality done

--- router/project.js
+++ router/project.js
@@ -13,4 +13,52 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+
+  Project.get(id)
+    .then(project => {
+      res.status(200).json({data: project});
+    })
+    .catch(err => {
+      res.status(500).json({message: err.message});
+    });
+});
+
+router.post('/', (req, res) => {
+  const data = req.body;
+  Project.insert(data)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({message: err.message});
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const {id} = req.params;
+  const changes = req.body;
+
+  Project.update(id, changes)
+    .then(project => {
+      res.status(200).json({project});
+    })
+    .catch(err => {
+      res.status(500).json({message: err.message});
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const {id} = req.params;
+
+  Project.remove(id)
+    .then(project => {
+      res.status(200).json({project});
+    })
+    .catch(err => {
+      res.status(500).json({message: err.message});
+    });
+});
+
 module.exports = router;
