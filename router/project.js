bbf3f7b75e807288cd9c2b117af0880592d70f45
Sunil Karki <karkisunil1200@gmail.com>
2020-05-08 12:11:11 -0500

    Getting project acitons by id

--- router/project.js
+++ router/project.js
@@ -25,6 +25,18 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.get('/:id/actions', (req, res) => {
+  const {id} = req.params;
+
+  Project.getProjectActions(id)
+    .then(action => {
+      res.status(200).json({data: action});
+    })
+    .catch(err => {
+      res.status(500).json({message: err.message});
+    });
+});
+
 router.post('/', (req, res) => {
   const data = req.body;
   Project.insert(data)
